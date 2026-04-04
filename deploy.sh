5f371e2dd2d10909f5d05ff90174f8b111065c77
timcoe429 <timcoe429@gmail.com>
2026-04-03 21:35:31 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@ ROOT="$(cd "$(dirname "$0")" && pwd)"
 cd "$ROOT/agent"
 git checkout -- package.json package-lock.json
 git pull
-npm install
 pm2 restart marketing-agent --update-env
 pm2 restart marketing-agent-webhook --update-env
